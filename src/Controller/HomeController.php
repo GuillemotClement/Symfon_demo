e2f01f85f4602661f938ba50f70e81a95bc4ed13
GuillemotClement <guillemot.clement@protonmail.com>
2025-08-04 16:20:01 +0200

    feat: add dump()

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
@@ -12,12 +13,14 @@ final class HomeController extends AbstractController
     // le premier argument est le path utiliser 
     // le second est le nom de la route qui permet d'y faire référence pour créer des liens.
     #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    public function index(string $name = '', Request $request): Response
     {
         $greet = '';
         if($name){
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
+
+        dump($request);
 
         return new Response('<html>
             <body>
